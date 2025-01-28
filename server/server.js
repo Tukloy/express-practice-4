0acc0736902bef7a724e522d5b34a0c4e229b9f8
Tukloy <rheyvince.26@gmail.com>
2025-01-28 20:04:18 +0800

    add the dist

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 import express from 'express'
 import cors from 'cors'
+import path from 'path';
+import { fileURLToPath } from 'url'
 import posts from './routes/posts.js'
 import error from './middleware/error.js'
 import notfound from './middleware/notfound.js'
@@ -7,12 +9,23 @@ import notfound from './middleware/notfound.js'
 const app = express()
 const port = process.env.PORT || 5000
 
+// Get the directory name of the current module
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
 // CORS middleware for mysql
 app.use(cors())
 
 // Body parser middleware
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+
+// Serve static
+app.use(express.static(path.join(__dirname, "dist")));
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
 
 // Routes
 app.use('/api/posts', posts)
